ba5a617ef7a6f4f70d402c784cdd90b9dd2f7400
highmachs <samarjeeth06@gmail.com>
2026-03-09 21:46:41 +0530

    Bust Nuxt static cache version for loader fix rollout

---
+++ _nuxt/static/1773071000/profile/state.js
@@ -0,0 +1,2 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1773071000",layout:"default",error:c,state:{modalOpen:b,imagesLoaded:!0,scroll:{scroll:{isScrolling:b,limit:{x:a,y:a},x:a,y:a}}},serverRendered:true,routePath:"\u002Fprofile",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:c}}}}(0,false,null));
+
